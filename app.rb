73c5c70bd8a62fa606ecfde875a9b8744cb42f38
“Brenda” <odonnell.brenda@gmail.com>
2019-02-21 11:46:35 +0000

    happy-birthday page displays Happy Birthday message to name inputted

--- app.rb
+++ app.rb
@@ -4,5 +4,15 @@ set :session_birthday, 'birthday'
 
 get '/' do
 "Hello there!"
-  erb :index 
+  erb :index
 end
+
+
+get '/happy-bday' do
+  @name = params[:name]
+  erb :wishes
+end
+
+   # p @name = params[:name]
+   # p @last_name = params[:last_name]
+   # p @age = params[:age]
