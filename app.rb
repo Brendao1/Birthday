2d4f5947faa733cd0cc5b8f9a996348e54236013
“Brenda” <odonnell.brenda@gmail.com>
2019-02-22 12:31:37 +0000

    Changed date formats

--- app.rb
+++ app.rb
@@ -1,7 +1,7 @@
 require 'date'
 require 'sinatra'
 set :session_birthday, 'birthday'
-
+CURRENT_YEAR = 2019
 
 get '/' do
 "Hello there!"
@@ -13,17 +13,15 @@ post '/happy-bday' do
   @name = params[:name]
   @day = params[:day]
   @month = params[:month]
+  @year = CURRENT_YEAR
+  # @bday = Date.new(@day.to_i, @month.to_i)
 
-  @bday = Date.new(@day.to_i, @month.to_i).strftime("%y, %m")
-  # @bday = @bday.strftime("%y, %m")
-
-  @today = Date.today.strftime("%d, %m")
+  @bday = Date.new(CURRENT_YEAR, @month.to_i, @day.to_i)
+  # @bday = Date.new(@day.to_i, @month.to_i).strftime("%d, %m")
+  @today = Date.today
+# @today = Date.today.strftime("%d, %m")
   # @today.strftime("%d, %m")
   erb :wishes
-
-  # if @bday == @today
-  #   redirect '/today'
-  # end
 
   # p @name
   # p @day
@@ -38,9 +36,9 @@ end
 
 
 # use this to print out the variables....
-  p @name
-  p @day
-  p @month
+  # p @name
+  # p @day
+  # p @month
   # p "The date today"
   # # p @today = Date.today
   # p @today.strftime("%d, %m")
